fdec52d826e4d73548598bd2cd494a9a1ceb847d
Grizi7 <mahmoudzidan7717@gmail.com>
2024-06-15 23:12:29 +0300

    organizing layout structure

--- layout/js/front-end.js
+++ layout/js/front-end.js
@@ -1,18 +1,20 @@
-// Form handling
-
-$('.appForm').find('input').keyup(function () {
-    var numberInput = $(this).val();
-    if (numberInput != "") {
-      var length = numberInput.length;
-      if (length >= 4) {
-        $(".details-handler").removeAttr("disabled");
-        $(".details-handler").removeClass("disabled");
-      } else {
-        $(".details-handler").attr("disabled", "disabled");
-        $(".details-handler").addClass("disabled");
-      }
+$(document).ready(function () {
+  $(".dropdown-item").click(function () {
+    var quality = $(this).text();
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+    $(".quality").text(quality);
+    if (quality == "1080p") {
+      $("img").addClass("d-none");
+      var video = $("video");
+      video.removeClass("d-none");
+      video[0].play();
     } else {
-      $(".details-handler").attr("disabled", "disabled");
-      $(".details-handler").addClass("disabled");
+      var video = $("video");
+      video[0].pause();
+      video.addClass("d-none");
+      $("img").removeClass("d-none");
+      $("img").attr("src", "layout/images/" + quality + ".png");
     }
+  });
 });
